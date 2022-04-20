4fb3c4587befa9ed135d9d5802a276cad9b32172
Cheol-Soon-Choi <79562672+Cheol-Soon-Choi@users.noreply.github.com>
2022-04-20 22:26:17 +0900

    Initial commit

---
+++ Service_A1/src/main/java/com/nyang/msatest/Service_A1Application.java
@@ -0,0 +1,13 @@
+package com.nyang.msatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service_A1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service_A1Application.class, args);
+    }
+
+}
